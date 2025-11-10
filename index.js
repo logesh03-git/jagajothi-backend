a66009559a6b4526e499d00f960f7e256611f7ef
logesh03-git <logeahlogesh06@gmail.com>
2025-11-10 16:24:36 +0530

    fix 1

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cors({
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "OPTIONS"],
 }));
 
 app.options('*', cors());
